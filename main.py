b2ab7d3923cc88a4534a4ddc9c42cc0593873183
NikolaiJamez <nikolaijamez@gmail.com>
2024-01-09 21:46:08 +1300

    Notes can be copied to clipbard

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import flet as ft
+import pyperclip as clip
 
 
 def main(page: ft.Page) -> None:
+
+    def copy_note(e: ft.ControlEvent) -> None:
+        clip.copy(e.control.content.value)
+        page.snack_bar.open = True
+        page.update()
 
     def add_note(e: ft.ControlEvent) -> None:
         notes_row.controls.append(
@@ -9,6 +15,7 @@ def main(page: ft.Page) -> None:
                 content = ft.Container(
                     content = ft.Text(note_text_field.value),
                     padding = 10,
+                    on_click = copy_note
                 ),
                 col = {'md': 4, 'lg': 3},
             )
@@ -31,6 +38,10 @@ def main(page: ft.Page) -> None:
     )
 
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.snack_bar = ft.SnackBar(
+        content = ft.Text('Note copied to clipboard'),
+        duration = 1000,
+    )
     page.add(
         ft.Divider(opacity = 0),
         ft.ResponsiveRow(
