6691819080f3b012aa100dc6ad2d3b9151fcdfe0
NikolaiJamez <nikolaijamez@gmail.com>
2024-01-09 21:23:16 +1300

    Added add note field

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ import flet as ft
 
 
 def main(page: ft.Page) -> None:
-    pass
+
+    note_text_field = ft.TextField(
+        label = 'Enter a new note here...',
+        max_length = 255,
+        multiline = True,
+        col = 8,
+    )
+
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(
+        ft.Divider(opacity = 0),
+        ft.ResponsiveRow(
+            [
+                note_text_field
+            ],
+            alignment = ft.MainAxisAlignment.CENTER
+        )
+    )
+    page.update()
 
 
 if __name__ == '__main__':
