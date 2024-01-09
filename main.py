6a0b5c1ad384547c7c3e9f6e87abf5bd157990c8
NikolaiJamez <nikolaijamez@gmail.com>
2024-01-09 21:58:06 +1300

    Added Navbar

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import pyperclip as clip
 
 def main(page: ft.Page) -> None:
 
+    def on_keyboard(e: ft.ControlEvent) -> None:
+        if e.shift and e.key == 'Enter':
+            add_note(e)
+
     def copy_note(e: ft.ControlEvent) -> None:
         clip.copy(e.control.content.value)
         page.snack_bar.open = True
@@ -37,10 +41,35 @@ def main(page: ft.Page) -> None:
         
     )
 
+    page.on_keyboard_event = on_keyboard
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.snack_bar = ft.SnackBar(
         content = ft.Text('Note copied to clipboard'),
         duration = 1000,
+    )
+    page.appbar = ft.AppBar(
+        leading = ft.Icon(ft.icons.EDIT_NOTE),
+        leading_width = 40,
+        title = ft.Text("Call Center Notes Manager"),
+        center_title = False,
+        bgcolor = ft.colors.SURFACE_VARIANT,
+        actions = [
+            ft.IconButton(
+                icon = ft.icons.SAVE,
+                icon_color = ft.colors.GREEN,
+                tooltip = 'Save Notes'
+            ),
+            ft.IconButton(
+                icon = ft.icons.REFRESH,
+                tooltip = 'Refresh Notes'
+            ),
+            ft.VerticalDivider(opacity = 0),
+            ft.IconButton(
+                icon = ft.icons.CLOSE,
+                icon_color = ft.colors.RED,
+                tooltip = 'Close App'
+            ),
+        ],
     )
     page.add(
         ft.Divider(opacity = 0),
